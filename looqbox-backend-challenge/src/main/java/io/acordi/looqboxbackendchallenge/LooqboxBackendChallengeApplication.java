914524eb9423e17e7708b078b238a6d7da95e92b
Pedro Acordi <pedroacordi2@gmail.com>
2024-12-30 15:29:19 -0300

    feat: set up project structure

---
+++ looqbox-backend-challenge/src/main/java/io/acordi/looqboxbackendchallenge/LooqboxBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package io.acordi.looqboxbackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooqboxBackendChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LooqboxBackendChallengeApplication.class, args);
+    }
+
+}
---
+++ looqbox-backend-challenge/src/test/java/io/acordi/looqboxbackendchallenge/LooqboxBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acordi.looqboxbackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooqboxBackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
